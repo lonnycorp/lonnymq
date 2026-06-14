b1b2b63dbf9a776de548e221042807f39143b534
Tim Lonsdale <timlonsdale@gmail.com>
2026-06-14 12:48:28 +0100

    [WWW] Pulled www/ into its own repository

--- www/src/component/markdown/break.ts
+++
@@ -1,9 +0,0 @@
-import { node } from "@lonnycorp/htmlforge"
-
-export class Break implements node.Buildable {
-    private readonly root = new node.Element("br")
-
-    build() {
-        return this.root.build()
-    }
-}
